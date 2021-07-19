853c070ca364e526d75579a71f3c2a18f8d7c6fa
joohyuk95 <jjang0306@naver.com>
2021-07-19 16:25:10 +0900

    2021-07-19

---
+++ sum_of_proper_divisors.cpp
@@ -0,0 +1,27 @@
+// 진약수의 합 (진약수 : 자기 자신을 제외한 양의 약수)
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n, i = 1, sum = 0;
+    cout << "input natural numer (3<N<=100)" << endl;
+    cin >> n;
+    
+    while (i < n) {
+        if (n % i == 0) {
+            sum += i;
+            if (i == 1) {
+                cout << i;
+            } else {
+                cout << " + " << i;
+            }
+        }
+        ++i;
+    }
+    cout << " = " << sum << endl;
+
+    return 0;    
+}
