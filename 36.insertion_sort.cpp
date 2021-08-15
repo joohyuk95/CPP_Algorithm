7aa031cb9896188f5310ed7b237fda3ad845dbba
joohyuk95 <jjang0306@naver.com>
2021-08-15 11:54:43 +0900

    2021-08-15

--- 36.insertion_sort.cpp
+++ 36.insertion_sort.cpp
@@ -13,11 +13,10 @@ int main()
     for (int i = 1; i < n; ++i) {
         key = nums[i];
         for (j = i-1; j >= 0; --j) {
-            if (nums[j] > key) {
+            if (nums[j] > key)
                 nums[j+1] = nums[j];
-            } else {
+            else
                 break;
-            }
         }
         nums[j+1] = key;
     }
