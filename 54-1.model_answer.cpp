0d929cdb0ae1e94d60b5a77c20061ee47115765c
joohyuk95 <jjang0306@naver.com>
2021-09-02 21:09:40 +0900

    2021-09-02

---
+++ 54-1.model_answer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack <char> s;
+    string str;
+    cin >> str;
+    for (auto x : str) {    // 범위 기반 for 문, for (element_declaration : array), auto는 타입추론 (알아서 형식 맞춰줌)
+        if (x == '(') s.push(x);
+        else {
+            if (s.empty() || s.top() != '(') {
+                cout << "NO";
+                return 0;
+            }
+            s.pop();
+        }
+    }
+    if (s.empty()) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
