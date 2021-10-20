2d0ed8f24cc9baea49a359457271eaeb4eb208c5
joohyuk95 <jjang0306@naver.com>
2021-10-21 00:38:36 +0900

    2021-10-20

--- p103.Floyd_Warshall.cpp
+++ p103.Floyd_Warshall.cpp
@@ -1,3 +1,4 @@
+// 플로이드-워샬 알고리즘
 // n개의 vertex와 m개의 edge로 가중치 방향그래프가 주어지면
 // 모든 vertex에서 모든 vertex로의 최소 비용을 행렬로 출력
 // 자기자신으로 가는 비용은 0, 갈 수 없으면 'M'을 출력한다.
@@ -13,7 +14,7 @@ int main()
     data.open("./p103_data.txt");
     int n, m, a, b, c;
     data >> n >> m;
-    vector<vector<int> > dp(n+1, vector<int>(n+1, 2000));
+    vector<vector<int> > dp(n+1, vector<int>(n+1, 2000));   // 2000 : inf 의미
     for (int i = 1; i <= n; ++i) dp[i][i] = 0;
     for (int i = 1; i <= m; ++i) {
         data >> a >> b >> c;
@@ -23,7 +24,7 @@ int main()
     for (int k = 1; k <= n; ++k) {
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
-                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);  // 기존의 i > j 비용과, k를 거쳐서 가는 i > k > j 중, 작은 값을 선택하면서 업데이트
             }
         }
     }
